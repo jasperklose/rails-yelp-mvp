9a32c48288b22e1e1851528995f878eb41783654
Jasper Klose <klosejasper@gmail.com>
2022-11-16 15:14:57 +0100

    done

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,34 @@
+class ReviewsController < ApplicationController
+  def index
+    @reviews = Review.all
+  end
+
+  def new
+    @review = Review.new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
+  def create
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new(review_params)
+    @review.restaurant = @restaurant
+    @review.save!
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  def show
+    @review = Review.find(params[:id])
+  end
+
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to reviews_path
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:rating, :content)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+Restaurant.delete_all
+puts "Creating Restaurants..."
+5.times do
+  Restaurant.create!(name: Faker::Restaurant.name, phone_number: Faker::PhoneNumber.cell_phone, category: "french", address: Faker::Address.full_address)
+end
+puts "Restaurants created..."
+
+all_restaurants = Restaurant.all
+
+all_restaurants.each do |restaurant|
+  Review.create!(rating: Faker::Number.within(range: 1..5), content: Faker::Restaurant.review, restaurant: restaurant)
+end
